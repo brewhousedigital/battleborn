a63eb97710161bf1351a0088921cf15da2d473d2
Brewhouse Digital <zach@brewhouse.digital>
2020-10-05 13:01:56 -0500

    Added new equipment area

--- _includes/js/character.js
+++ _includes/js/character.js
@@ -29,14 +29,53 @@ function loadCharacterInfo() {
 			"int": 5,
 			"wis": 7,
 			"cha": 3,
+			"weapon": {
+				"name": "Sharpened Sword",
+				"description": "This weapon was crafted by a skilled blacksmith.",
+				"image": "placeholder",
+				"damageDice": "1d8",
+				"damageBonus": 2,
+				"physicalArmor": 0,
+				"magicArmor": 0,
+				"price": 50
+			},
+			"armor": {
+				"name": "Light Armor",
+				"description": "This armor provides slight protection against physical attacks.",
+				"image": "placeholder",
+				"damageDice": "",
+				"damageBonus": 2,
+				"physicalArmor": 0,
+				"magicArmor": 0,
+				"price": 20
+			},
+			"accessory": {
+				"name": "Infinity Ring of Power",
+				"description": "Allows you to add +50 to your strength, dexterity, and constitution. Can only be used once per day.",
+				"image": "placeholder",
+				"damageDice": "",
+				"damageBonus": 0,
+				"physicalArmor": 0,
+				"magicArmor": 0,
+				"price": 5000
+			}
 		};
+
+		console.log(response);
 
 		document.querySelectorAll("[data-load]").forEach(function(el) {
 			let dataLoad = el.getAttribute("data-load");
-			if(!el.hasAttribute("data-static")) {
-				el.innerText = response[dataLoad];
-			}
+			el.innerText = response[dataLoad];
 		});
+
+
+
+
+
+
+
+
+
 
 
 		// Health slider
@@ -72,6 +111,14 @@ function loadCharacterInfo() {
 				healthSlider.dispatchEvent(event);
 			})
 		});
+
+
+
+
+
+
+
+
 
 
 
@@ -119,6 +166,65 @@ function loadCharacterInfo() {
 				}
 			});
 		});
+
+
+
+
+
+
+
+		// Equipment
+		let equipment = document.querySelectorAll("[data-equipment]");
+
+		// Backendless
+		// let queryBuilder = Backendless.DataQueryBuilder.create();
+		// queryBuilder.addProperties('name');
+		// queryBuilder.addProperties('type.name as type');
+		// queryBuilder.addProperties('damageDice.name as damageDice');
+		/*Backendless.Data.of("d20_equipment").find(queryBuilder)
+			.then( function(response) {
+				console.log(response);
+			})
+			.catch( function(error) {
+				console.log(error);
+			});*/
+
+		equipment.forEach(function(item) {
+			let type = item.getAttribute("data-equipment");
+
+			item.innerHTML = `
+				<img src="/images/equipment/equipment-${type}-${response[type]["image"]}.png"
+				 	 alt="${response[type]["name"]}"
+				 	 title="${response[type]["name"]}"
+				 	 class="">
+			`;
+
+			item.addEventListener("click", function() {
+				let equipmentDetails = document.getElementById("equipment-details");
+				equipmentDetails.style.display = "block";
+
+				let equipmentDetailsList = equipmentDetails.querySelectorAll("[data-equipment-details]");
+
+				equipmentDetailsList.forEach(function(line) {
+					let lineType = line.getAttribute("data-equipment-details");
+					line.style.display = "block";
+
+					if(typeof response[type][lineType] === "string") {
+						if(response[type][lineType].length > 0) {
+							line.querySelector("span").innerText = response[type][lineType];
+						} else {
+							line.style.display = "none";
+						}
+					}
+
+					if(typeof response[type][lineType] === "number") {
+						line.querySelector("span").innerText = response[type][lineType];
+					}
+
+				});
+			});
+		});
+
 	}
 }
 
